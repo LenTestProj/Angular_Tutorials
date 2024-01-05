c5cecf309628d526b5cb344f6bbde39980c6b343
retrovibe51 <w.barretto.code@gmail.com>
2024-01-05 18:06:09 +0530

    finished Section 5 at Video 88

--- Section5_Components_Databinding_DeepDive/component-databinding-deepDive/src/app/server-element/server-element.component.ts
+++ Section5_Components_Databinding_DeepDive/component-databinding-deepDive/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit,OnChanges, ViewEncapsulation, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit,OnChanges, ViewEncapsulation, SimpleChange, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestroy, ViewChild, ElementRef, AfterViewInit, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,30 @@ import { Component, Input, OnInit,OnChanges, ViewEncapsulation, SimpleChange, Si
   styleUrls: ['./server-element.component.css'],
 //   encapsulation:ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit,OnChanges {
+export class ServerElementComponent implements OnInit,OnChanges,DoCheck,AfterContentInit,AfterContentChecked,OnDestroy,AfterViewInit {
     @Input('srvElement') element:{type:string, name:string, content:string}={type:'',name:'',content:''};
     @Input() name:string="";
-    
+    @ViewChild('heading') header:any;
+    @ContentChild('contentParagraph') paragraph:any;
     constructor() { 
         console.log("constructor called");
+    }
+    ngAfterViewInit(): void {
+        console.log("ng After view init called");
+        console.log('header text: ',this.header.nativeElement.textContent);
+    }
+    ngOnDestroy(): void {
+        console.log("ng on Destroy called")
+    }
+    ngAfterContentChecked(): void {
+        console.log("ng after content checked called") //called after change detection cycle. called after ngDoCheck in this case
+    }
+    ngAfterContentInit(): void {
+        console.log("ng after content Init called")  //function called only once
+        console.log('Text Content Paragraph - ',this.paragraph.nativeElement.textContent);
+    }
+    ngDoCheck(): void {
+        console.log("ngDoCheck called")
     }
 
     ngOnChanges(changes:SimpleChanges){
@@ -21,6 +39,7 @@ export class ServerElementComponent implements OnInit,OnChanges {
 
     ngOnInit(): void {
         console.log("ngOnInit called");
+        // console.log('header text: ',this.header.nativeElement.textContent);
     }
 
 }
