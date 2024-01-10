cc7953ebc600668f7d21a7f8d61251a69e107411
retrovibe51 <w.barretto.code@gmail.com>
2024-01-10 18:02:25 +0530

    finished till video 122 - Using a service for cross component Communication

--- Section10_Course_Project_Services_Dependency_Injection/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Section10_Course_Project_Services_Dependency_Injection/ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.services';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,18 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-    @Output() recipeWasSelected=new EventEmitter<Recipe>();
-    recipes:Recipe[]=[
-        new Recipe('A Test Recipe', 'This is simply a path','https://imgs.search.brave.com/aI53ZdoesOwT5DXHsYLt0F5EyxN21JgIJspDgaTeUpI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjAz/OTA2NDg0L3Bob3Rv/L3ZlZ2V0YWJsZS1z/YWxhZC5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9ZjdCbkpS/Q3FMS2FqX0RFUUIx/U0I3MV9lUlQ4eTFY/UlA1MmREeVlSU3h1/RT0'),
-        new Recipe('Another Test Recipe', 'This is simply a path','https://imgs.search.brave.com/aI53ZdoesOwT5DXHsYLt0F5EyxN21JgIJspDgaTeUpI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjAz/OTA2NDg0L3Bob3Rv/L3ZlZ2V0YWJsZS1z/YWxhZC5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9ZjdCbkpS/Q3FMS2FqX0RFUUIx/U0I3MV9lUlQ4eTFY/UlA1MmREeVlSU3h1/RT0')
-    ];
-    constructor() { }
+    recipes:Recipe[]|undefined;
+    
+    constructor(private recipeService:RecipeService) { }
 
     ngOnInit(): void {
+        this.recipes = this.recipeService.getRecipes();
     }
 
-    onRecipeSelected(recipe:Recipe){
-        this.recipeWasSelected.emit(recipe);
-    }
+    
 
 }
