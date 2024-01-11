0663ba17b5e6f8c68327820cc04689b9975104ac
retrovibe51 <w.barretto.code@gmail.com>
2024-01-11 18:11:24 +0530

    done with Section 10

--- Section10_Course_Project_Services_Dependency_Injection/ng4-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Section10_Course_Project_Services_Dependency_Injection/ng4-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,16 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Injectable, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.services';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
+
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe:Recipe|undefined;  
-  constructor() { }
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
   }
 
+  onAddShoppingList(){
+    this.recipeService.addIngredientsToShoppingList(this.recipe?.ingredients!)
+  }
 }
