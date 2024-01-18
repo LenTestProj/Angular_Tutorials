3dfa16403767b600a5d58442f3ec5b32f9e2e292
LenTestProj <lennfernz2@gmail.com>
2024-01-19 05:23:28 +0530

    Done with Section 14 - Course Project: Onservables

---
+++ Section14_CourseProject_Observables/ng4-complete-guide/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.services';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit,OnDestroy {
+    ingredients:Ingredient[]|undefined;
+    private igChangedSub:Subscription|undefined;
+
+    constructor(private slService:ShoppingListService) { }
+
+    ngOnInit(): void {
+        this.ingredients=this.slService.getIngredients();
+        this.igChangedSub=this.slService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
+            this.ingredients=ingredients;
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.igChangedSub?.unsubscribe()
+    }
+}
