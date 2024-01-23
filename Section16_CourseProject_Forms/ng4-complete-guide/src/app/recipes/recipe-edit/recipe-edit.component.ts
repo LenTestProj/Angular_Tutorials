53429f0d37eea17e9d26d6f6dc0f7522a67f5124
retrovibe51 <w.barretto.code@gmail.com>
2024-01-23 18:00:09 +0530

    Started with Section 17 - Using Pipes to transofrm Output

--- Section16_CourseProject_Forms/ng4-complete-guide/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Section16_CourseProject_Forms/ng4-complete-guide/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.services';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -12,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
     id:number|undefined;
     editMode=false;
 
-    constructor(private route:ActivatedRoute,private recipeService:RecipeService) { }
+    constructor(private route:ActivatedRoute,private recipeService:RecipeService,private router:Router) { }
 
     recipeForm:FormGroup=new FormGroup({});
 
@@ -25,7 +26,14 @@ export class RecipeEditComponent implements OnInit {
     }
 
     onSubmit(){
-        console.log(this.recipeForm);    
+        // const newRecipe=new Recipe(this.recipeForm.value['name'],this.recipeForm.value['description'],this.recipeForm.value['imagePath'],this.recipeForm.value['ingredients']);
+        if(this.editMode){
+           this.recipeService.updateRecipe(this.id!,this.recipeForm.value);
+        }
+        else{
+            this.recipeService.addRecipes(this.recipeForm.value);
+        }
+        this.onCancel();
     }
 
     private initForm(){
@@ -42,17 +50,17 @@ export class RecipeEditComponent implements OnInit {
             if(recipe['ingredients']){
                 for(let ingredient of recipe.ingredients){
                     recipeIngredients.push(new FormGroup({
-                        'name':new FormControl(ingredient.name),
-                        'amount':new FormControl(ingredient.amount)
+                        'name':new FormControl(ingredient.name,Validators.required),
+                        'amount':new FormControl(ingredient.amount,[Validators.required,Validators.pattern(/^[1-9]+[0-9]*$/)])
                     }))
                 }
             }
         }
 
         this.recipeForm=new FormGroup({
-            'name':new FormControl(recipeName),
-            'imagePath':new FormControl(recipeImageUrl),
-            'description':new FormControl(recipeDescription),
+            'name':new FormControl(recipeName,Validators.required),
+            'imagePath':new FormControl(recipeImageUrl, Validators.required),
+            'description':new FormControl(recipeDescription,Validators.required),
             'ingredients':recipeIngredients
         })
     }
@@ -64,10 +72,17 @@ export class RecipeEditComponent implements OnInit {
     onAddIngredient(){
         (<FormArray>this.recipeForm.get('ingredients')).push(
             new FormGroup({
-                'name':new FormControl(),
-                'amount':new FormControl()
+                'name':new FormControl(null,Validators.required),
+                'amount':new FormControl(null,[Validators.required,Validators.pattern(/^[1-9]+[0-9]*$/)])
             })
         )   
     }
 
+    onCancel(){
+        this.router.navigate(['../'],{relativeTo:this.route})
+    }
+
+    onDeleteIngredient(index:number){
+        (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    }
 }
--- Section16_CourseProject_Forms/ng4-complete-guide/src/app/recipes/recipe.services.ts
+++ Section16_CourseProject_Forms/ng4-complete-guide/src/app/recipes/recipe.services.ts
@@ -6,7 +6,8 @@ import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService{
-    
+    recipesChanged=new Subject<Recipe[]>()
+
     private recipes:Recipe[]=[
         new Recipe('A Test Recipe', 'This is simply a path','https://imgs.search.brave.com/aI53ZdoesOwT5DXHsYLt0F5EyxN21JgIJspDgaTeUpI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjAz/OTA2NDg0L3Bob3Rv/L3ZlZ2V0YWJsZS1z/YWxhZC5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9ZjdCbkpS/Q3FMS2FqX0RFUUIx/U0I3MV9lUlQ4eTFY/UlA1MmREeVlSU3h1/RT0',[
             new Ingredient('meat',1),
@@ -30,5 +31,20 @@ export class RecipeService{
 
     getRecipe(index:number){
         return this.recipes[index];
+    }
+
+    addRecipes(recipe:Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index:number,recipe:Recipe){
+        this.recipes[index]=recipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index:number){
+        this.recipes.splice(index,1);
+        this.recipesChanged.next(this.recipes.slice())   
     }
 }
