3f96cf78f5c0e3f10cf93888f119161c6e547089
retrovibe51 <w.barretto.code@gmail.com>
2024-01-24 18:09:00 +0530

    started with Section 18 - making HTTP requests

--- Section17_Using_Pipe_Transform_Output/ng4-complete-guide/src/app/app.component.ts
+++ Section17_Using_Pipe_Transform_Output/ng4-complete-guide/src/app/app.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers = [
+    appStatus=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve('stable');
+        },2000)
+    })
+    servers = [
     {
       instanceType: 'medium',
       name: 'Production Server',
