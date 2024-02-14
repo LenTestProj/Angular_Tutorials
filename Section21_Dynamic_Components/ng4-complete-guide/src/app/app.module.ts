320e8aa1ac0b7449db37832c18ac0a3798d74292
LenTestProj <lennfernz2@gmail.com>
2024-02-15 05:22:23 +0530

    done with Section 21 - Dynamic Components

--- Section21_Dynamic_Components/ng4-complete-guide/src/app/app.module.ts
+++ Section21_Dynamic_Components/ng4-complete-guide/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinner } from './shared/loading-Spinner/loading-spinner';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceHolderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { AlertComponent } from './shared/alert/alert.component';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinner,
-    AlertComponent
+    AlertComponent,
+    PlaceHolderDirective
   ],
   imports: [
     BrowserModule,
@@ -47,6 +49,6 @@ import { AlertComponent } from './shared/alert/alert.component';
     HttpClientModule
   ],
   providers: [ShoppingListService,RecipeService, {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true}],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- Section21_Dynamic_Components/ng4-complete-guide/src/app/auth/auth.component.ts
+++ Section21_Dynamic_Components/ng4-complete-guide/src/app/auth/auth.component.ts
@@ -1,20 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
+import {AlertComponent} from '../shared/alert/alert.component';
+import { PlaceHolderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
     selector:'app-auth',
     templateUrl:'./auth.component.html'
 })
-export class AuthComponent{
+export class AuthComponent implements OnDestroy{
     isLoginMode=true;   
     IsLoading=false;
-    error:string='';
+    error:string|null='';
+    @ViewChild(PlaceHolderDirective) alertHost:PlaceHolderDirective|undefined
 
+    private closeSub:Subscription=new Subscription();
 
-    constructor(private authService:AuthService, private router:Router){}
+    constructor(private authService:AuthService, private router:Router, private componentFactoryResolver:ComponentFactoryResolver){}
 
 
     onSwitchMode(){
@@ -47,9 +51,40 @@ export class AuthComponent{
             console.log(error);
               this.error=error.
               message;
+              this.showErrorAlert(error)
               this.IsLoading=false;
         }}) 
 
         form.reset();
+    }
+
+    OnHandleError(){
+      this.error=null;  
+    }
+
+    ngOnDestroy(): void {
+        if(this.closeSub){
+            this.closeSub.unsubscribe();
+        }
+    }
+
+    private showErrorAlert(message:string){
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent)///dynamically load the component
+
+        const hostViewContainerRef=this.alertHost?.viewContainerRef;
+        hostViewContainerRef?.clear(); //in case other components have used this ref.
+
+        const componentRef = hostViewContainerRef?.createComponent(alertCmpFactory);
+
+        if(componentRef)
+        {
+            componentRef.instance.message=message;
+            this.closeSub = componentRef.instance.close.subscribe(()=>{
+this.closeSub.unsubscribe();
+hostViewContainerRef?.clear()
+            })
+        }
+
+
     }
 }
