185cfce45fd9cda26fbe357af07c1287df3e9908
LenTestProj <lennfernz2@gmail.com>
2024-02-27 05:23:46 +0530

    donw with section 399 - Selecting State

--- Section26_Using_Ngrx/Recipe Project/ng4-complete-guide/src/app/shopping-list/shopping-list.component.ts
+++ Section26_Using_Ngrx/Recipe Project/ng4-complete-guide/src/app/shopping-list/shopping-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { LoggingService } from '../logging.service';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,22 +12,20 @@ import { LoggingService } from '../logging.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[]=[new Ingredient('',0)];
+  ingredients: Observable<{ingredients:Ingredient[]}>|undefined;
   private subscription: Subscription=new Subscription();
 
   constructor(
     private slService: ShoppingListService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    private store:Store<{shoppingList:{
+        ingredients:Ingredient[]
+    }}>
   ) {}
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
-
+    this.ingredients=this.store.select('shoppingList');
+    
     this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
   }
 
@@ -35,6 +34,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    // this.subscription.unsubscribe();
   }
 }
