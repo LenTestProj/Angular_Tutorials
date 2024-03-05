f5b7acfdb6f271b7e2e50d97f1244726015f5095
retrovibe51 <w.barretto.code@gmail.com>
2024-03-05 18:02:47 +0530

    Reached video 414 - Login via @ngrx/effects

--- Section26_Using_Ngrx/Recipe Project/ng4-complete-guide/src/app/auth/store/auth.actions.ts
+++ Section26_Using_Ngrx/Recipe Project/ng4-complete-guide/src/app/auth/store/auth.actions.ts
@@ -1,7 +1,8 @@
 import { Action } from "@ngrx/store";
 
-export const LOGIN="LOGIN";
-export const LOGOUT="LOGOUT";
+export const LOGIN_START='[Auth] LOGIN START'
+export const LOGIN="[Auth] LOGIN";
+export const LOGOUT="[Auth] LOGOUT";
 
 export class Login implements Action{
     readonly type=LOGIN;
@@ -15,6 +16,11 @@ export class Login implements Action{
 
 export class Logout implements Action{
     readonly type=LOGOUT;
+}
+
+export class LoginStart implements Action {
+    readonly type=LOGIN_START;
+    constructor(public payload:{email:string,password:string}){};
 }
 
 export type AuthActions=Login|Logout;
