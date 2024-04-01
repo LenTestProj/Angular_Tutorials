bd6c3f7d922079abaec9b193a34ee61ac2e01804
LenTestProj <lennfernz2@gmail.com>
2024-04-02 05:25:01 +0530

    finished video 467 - testing dependencies - components and services

--- Section28_Angular_Animations/angular_animations/src/app/app.component.ts
+++ Section28_Angular_Animations/angular_animations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -33,7 +33,75 @@ import { Component } from '@angular/core';
         })),
         transition('normal => highlighted',animate(300)),
         transition('normal => highlighted',animate(800)),
-        transition('shrunken <=> *',animate(500))
+        transition('shrunken <=> *',[
+            style({
+                'backgroundColor':'orange'
+            }),
+            animate(1000,style({
+                borderRadius:'50px'
+            })),
+            animate(500)
+        ])
+    ]),
+    trigger('list1',[
+        state('in',style({
+            opacity:1,
+            transform:'translateX(0)'
+        })),
+        transition('void => *',[
+            style({
+                opacity:0,
+                // transform:'translateX(-100px)'
+            }),
+            animate(700)
+        ]),  //void is a state before the element is applied to the DOM,
+        transition('* => void',[
+            animate(300,style({
+                transform:'translateX(100px)',
+                opacity:0
+            }))
+        ]) 
+    ]),
+    trigger('list2',[
+        state('in',style({
+            opacity:1,
+            transform:'translateX(0)'
+        })),
+        transition('void => *',[
+           animate(1000,keyframes([
+            style({
+                transform:'translateX(-100px)',
+                opacity:0,
+                offset:0
+            }),
+            style({
+                transform:'translateX(-50px)',
+                opacity:0.5,
+                offset:0.3
+            }),
+            style({
+                transform:'translateX(-20px)',
+                opacity:1,
+                offset:0.8
+            }),
+            style({
+                transform:'translateX(0px)',
+                opacity:1,
+                offset:1
+            })
+           ])) 
+        ]),  //void is a state before the element is applied to the DOM,
+        transition('* => void',[
+            group([
+                animate(700,style({
+                    color:'red'
+                })),
+                animate(700,style({
+                    transform:'translateX(100px)',
+                    opacity:0
+                })), 
+            ])        
+        ]) 
     ])
   ]
 })
@@ -57,5 +125,13 @@ export class AppComponent {
 
     onDelete(item:String){
         this.list.splice(this.list.indexOf(item),1);
+    }
+
+    animationStarted(event:any){
+        console.log(event);
+    }
+
+    animationEnded(event:any){
+        console.log(event);
     }
 }
---
+++ Section30_Angular_Unit_Testing/angular_unit_testing/src/app/user/user.component.spec.ts
@@ -0,0 +1,67 @@
+import { ComponentFixture, TestBed, async, waitForAsync } from '@angular/core/testing';
+
+import { UserComponent } from './user.component';
+import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
+
+describe('UserComponent', () => {
+    beforeEach(()=>{
+        TestBed.configureTestingModule({
+            declarations:[UserComponent]
+        });
+    })
+
+    it("should create the app",()=>{
+        let fixture=TestBed.createComponent(UserComponent);
+        let app=fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy()
+    })
+
+    it('should use the user name from the service',()=>{
+        let fixture=TestBed.createComponent(UserComponent);
+        let app=fixture.debugElement.componentInstance;
+        let userService=fixture.debugElement.injector.get(UserService);
+        fixture.detectChanges() //detect changes after injection of user service
+        expect(userService.user.name).toEqual(app.user.name)
+    })
+
+    it("should display the user name if user is logged in",()=>{
+        let fixture=TestBed.createComponent(UserComponent);
+        let app=fixture.debugElement.componentInstance; 
+        app.isLoggedIn=true;
+        fixture.detectChanges();
+        let compiled=fixture.debugElement.nativeElement;   
+        expect(compiled.querySelector('p').textContent).toContain(app.user.name)
+    })
+
+    it("should not display the user name if user is logged in",()=>{
+        let fixture=TestBed.createComponent(UserComponent);
+        let app=fixture.debugElement.componentInstance; 
+        fixture.detectChanges();
+        let compiled=fixture.debugElement.nativeElement;   
+        expect(compiled.querySelector('p').textContent).not.toContain(app.user.name)
+    });
+
+    it('shouldn\'t fetch data successfully if not called asynchronously',()=>{
+        let fixture=TestBed.createComponent(UserComponent);
+        let app=fixture.debugElement.componentInstance; 
+        let dataService=fixture.debugElement.injector.get(DataService); 
+        let spy=spyOn(dataService,'getDetails').and.returnValue(Promise.resolve('data'));  //spyon listens for events on the data service method getDetails adn retruns a mock value
+        fixture.detectChanges();
+        console.log(app.data);
+        expect(app.data).toBe('')
+    })
+
+    it('should fetch data successfully if not called asynchronously',waitForAsync(()=>{
+        let fixture=TestBed.createComponent(UserComponent);
+        let app=fixture.debugElement.componentInstance; 
+        let dataService=fixture.debugElement.injector.get(DataService); 
+        let spy=spyOn(dataService,'getDetails').and.returnValue(Promise.resolve('data'));  //spyon listens for events on the data service method getDetails adn retruns a mock value
+        fixture.detectChanges();
+        fixture.whenStable().then(()=>{
+          expect(app.data).toBe(undefined)  
+        });//reacts when all asynchronous tasks are finished
+        console.log(app.data);
+        expect(app.data).toBe('')
+    }))
+});
